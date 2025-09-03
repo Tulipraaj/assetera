a72b86e9621f081b9700b49f0843fb78b8b0fd6e
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2025-09-03 16:10:08 +0530

    Update loader_ASSET_OBJECTIVES.sql

--- data/loader_ASSET_OBJECTIVES.sql
+++ data/loader_ASSET_OBJECTIVES.sql
@@ -3,4 +3,3 @@ COPY INTO ASSET_OBJECTIVES (ASSET_OBJECTIVE_NAME)
 FROM @repo_stage/Asset_Objectives.csv
 FILE_FORMAT = (TYPE=CSV SKIP_HEADER=1 FIELD_OPTIONALLY_ENCLOSED_BY='"')
 ON_ERROR = 'ABORT_STATEMENT';
---------------------------------------------------------
