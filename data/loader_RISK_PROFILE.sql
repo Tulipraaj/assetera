a591bf54d5670f86118b984f1f5daf90a3c6980b
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2025-09-03 16:17:18 +0530

    Update loader_RISK_PROFILE.sql

--- data/loader_RISK_PROFILE.sql
+++ data/loader_RISK_PROFILE.sql
@@ -3,4 +3,4 @@ COPY INTO RISK_PROFILE (RISK_PROFILE)
 FROM @repo_stage/Risk_Profile.csv
 FILE_FORMAT = (TYPE=CSV SKIP_HEADER=1 FIELD_OPTIONALLY_ENCLOSED_BY='"')
 ON_ERROR = 'ABORT_STATEMENT';
---------------------------------------------------------
+
