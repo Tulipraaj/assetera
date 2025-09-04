91776a5c6ac0403540d66344053ae27e32936b7c
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2025-09-04 10:58:52 +0530

    Update fn_fund_revenue_per_customer.sql

--- functions/fn_fund_revenue_per_customer.sql
+++ functions/fn_fund_revenue_per_customer.sql
@@ -1,35 +1,26 @@
-
-CREATE OR REPLACE FUNCTION fn_fund_revenue_per_customer (p_Fund_ID INT)
-RETURN fund_revenue_table PIPELINED
+CREATE OR REPLACE FUNCTION fn_fund_revenue_per_customer(p_Fund_ID INT)
+RETURNS TABLE (
+    Fund_ID INT,
+    Fund_Name STRING,
+    Min_Revenue_Per_Customer NUMBER,
+    Max_Revenue_Per_Customer NUMBER
+)
 AS
-BEGIN
-    FOR rec IN (
-        SELECT
-            pf.Fund_ID,
-            pf.Fund_Name,
-            SUM(
-                fs.Flat_Fee + 
-                (fs.Percentage_Fee / 100.0) * pf.Minimum_Investment_Required * fa.Percent_Of_Fund / 100.0
-            ) AS Min_Revenue_Per_Customer,
-            SUM(
-                fs.Flat_Fee + 
-                (fs.Percentage_Fee / 100.0) * pf.Maximum_Investment_Allowed * fa.Percent_Of_Fund / 100.0
-            ) AS Max_Revenue_Per_Customer
-        FROM Potential_Funds pf
-        JOIN Fund_Assets fa ON pf.Fund_ID = fa.Fund_ID
-        JOIN Fee_Structure fs ON fa.Asset_Class_ID = fs.Asset_Class_ID
-        WHERE pf.Fund_ID = p_Fund_ID
-        GROUP BY pf.Fund_ID, pf.Fund_Name
-    ) LOOP
-        PIPE ROW (
-            fund_revenue_record(
-                rec.Fund_ID,
-                rec.Fund_Name,
-                rec.Min_Revenue_Per_Customer,
-                rec.Max_Revenue_Per_Customer
-            )
-        );
-    END LOOP;
-    RETURN;
-END;
-/
+$$
+    SELECT
+        pf.Fund_ID,
+        pf.Fund_Name,
+        SUM(
+            fs.Flat_Fee + 
+            (fs.Percentage_Fee / 100.0) * pf.Minimum_Investment_Required * fa.Percent_Of_Fund / 100.0
+        ) AS Min_Revenue_Per_Customer,
+        SUM(
+            fs.Flat_Fee + 
+            (fs.Percentage_Fee / 100.0) * pf.Maximum_Investment_Allowed * fa.Percent_Of_Fund / 100.0
+        ) AS Max_Revenue_Per_Customer
+    FROM Potential_Funds pf
+    JOIN Fund_Assets fa ON pf.Fund_ID = fa.Fund_ID
+    JOIN Fee_Structure fs ON fa.Asset_Class_ID = fs.Asset_Class_ID
+    WHERE pf.Fund_ID = p_Fund_ID
+    GROUP BY pf.Fund_ID, pf.Fund_Name
+$$;
