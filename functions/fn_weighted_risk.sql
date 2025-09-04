9ef61cce2bfb5247b10517bb79a00fa701055d21
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2025-09-04 11:06:33 +0530

    Update fn_weighted_risk.sql

--- functions/fn_weighted_risk.sql
+++ functions/fn_weighted_risk.sql
@@ -1,15 +1,10 @@
---Calculating the weighted average risk score of your fund samples
-CREATE OR REPLACE FUNCTION fn_weighted_risk (fund_id IN NUMBER)
-RETURN NUMBER
-IS
-    v_weighted_score NUMBER;
-BEGIN
+CREATE OR REPLACE FUNCTION fn_weighted_risk(fund_id INT)
+RETURNS NUMBER
+AS
+$$
     SELECT ROUND(SUM((fa.percent_of_fund/100) * ac.risk_profile_id))
-    INTO v_weighted_score
     FROM fund_assets fa
-    JOIN asset_classes ac ON fa.asset_class_id = ac.asset_class
-    WHERE fa.fund_id = fund_id;
-
-    RETURN v_weighted_score;
-END;
-/
+    JOIN asset_classes ac 
+        ON fa.asset_class_id = ac.asset_class
+    WHERE fa.fund_id = fund_id
+$$;
