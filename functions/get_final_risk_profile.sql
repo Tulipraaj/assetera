c27d897f3da49975943aa5dea64c323787ae2eb6
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2025-09-08 11:55:39 +0530

    Update get_final_risk_profile.sql to js

--- functions/get_final_risk_profile.sql
+++ functions/get_final_risk_profile.sql
@@ -1,37 +1,31 @@
-CREATE OR REPLACE FUNCTION get_final_risk_profile(p_customer_id INT)
+CREATE OR REPLACE FUNCTION get_final_risk_profile_js(p_customer_id NUMBER)
 RETURNS NUMBER
+LANGUAGE JAVASCRIPT
 AS
-$$
-    (
-        SELECT CASE
-                   WHEN mode_risk IN (1,2) THEN FLOOR(avg_risk)
-                   WHEN mode_risk IN (4,5) THEN CEIL(avg_risk)
-                   ELSE ROUND(avg_risk)
-               END
-        FROM (
-            SELECT 
-                AVG(rp.risk_profile_id) AS avg_risk,
-                (
-                    SELECT rp2.risk_profile_id
-                    FROM customer_answers ca2
-                    JOIN answers a2
-                      ON ca2.question_id = a2.question_id
-                     AND ca2.answer_id = a2.answer_id
-                    JOIN risk_profile rp2
-                      ON a2.risk_profile_id = rp2.risk_profile_id
-                    WHERE ca2.customer_id = ca.customer_id
-                    GROUP BY rp2.risk_profile_id
-                    ORDER BY COUNT(*) DESC, rp2.risk_profile_id ASC
-                    LIMIT 1
-                ) AS mode_risk
-            FROM customer_answers ca
-            JOIN answers a
-              ON ca.question_id = a.question_id
-             AND ca.answer_id = a.answer_id
-            JOIN risk_profile rp
-              ON a.risk_profile_id = rp.risk_profile_id
-            WHERE ca.customer_id = p_customer_id
-            GROUP BY ca.customer_id
-        ) t
-    )
-$$;
+'
+var stmt1 = snowflake.createStatement({
+    sqlText: "SELECT AVG(rp.risk_profile_id) as avg_risk FROM customer_answers ca JOIN answers a ON ca.question_id = a.question_id AND ca.answer_id = a.answer_id JOIN risk_profile rp ON a.risk_profile_id = rp.risk_profile_id WHERE ca.customer_id = " + p_customer_id
+})
+
+var stmt2 = snowflake.createStatement({
+    sqlText: "SELECT rp.risk_profile_id FROM customer_answers ca JOIN answers a ON ca.question_id = a.question_id AND ca.answer_id = a.answer_id JOIN risk_profile rp ON a.risk_profile_id = rp.risk_profile_id WHERE ca.customer_id = " + p_customer_id + " GROUP BY rp.risk_profile_id ORDER BY COUNT(*) DESC, rp.risk_profile_id ASC LIMIT 1"
+})
+
+var result1 = stmt1.execute()
+var result2 = stmt2.execute()
+
+if (!result1.next() || !result2.next()) {
+    return null
+}
+
+var avg_risk = result1.getColumnValue(1)
+var mode_risk = result2.getColumnValue(1)
+
+if (mode_risk == 1 || mode_risk == 2) {
+    return Math.floor(avg_risk)
+} else if (mode_risk == 4 || mode_risk == 5) {
+    return Math.ceil(avg_risk)
+} else {
+    return Math.round(avg_risk)
+}
+';
